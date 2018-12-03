ebf00af374e12212b7011325dc8e5f8cd4506e9c
MartinaSp <>
2018-12-03 20:49:24 +0000

    FE done, ale nie moc pekny

--- src/Controller/ProductDetailController.php
+++ src/Controller/ProductDetailController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Form\ProductType;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductDetailController extends AbstractController
@@ -10,10 +15,15 @@ class ProductDetailController extends AbstractController
     /**
      * @Route("/product/detail", name="product_detail")
      */
-    public function index()
+    public function index(ProductRepository $productRepository): Response
     {
-        return $this->render('product_detail/index.html.twig', [
-            'controller_name' => 'ProductDetailController',
-        ]);
+    return $this->render('product_detail/index.html.twig', ['products' => $productRepository->findAll()]);
     }
+    
+    // public function index()
+    // {
+    //    return $this->render('product_detail/index.html.twig', [
+    //       'controller_name' => 'ProductDetailController',
+    //    ]);
+    // }
 }
